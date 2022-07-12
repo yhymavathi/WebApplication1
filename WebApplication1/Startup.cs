70142c851fb4ea4857c31a5596373c56ec89d16b
Yalla Hymavathi <v-yallah@microsoft.com>
2022-07-12 15:50:20 +0530

    updated changes

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -33,7 +33,7 @@ namespace WebApplication1
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
-                    await context.Response.WriteAsync("Welcome");
+                    await context.Response.WriteAsync("Good Morning");
 
                 });
             });
